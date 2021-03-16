492b4882874b57a31a2ce5d44ff305ac3c69f60e
Atndesign <Atnnetworkk@gmail.com>
2021-03-16 17:27:42 +0100

    Add the intro component htmll

---
+++ src/components/main.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <main className="intro">
+        <h1>Saxophone</h1>
+      </main>
+    );
+  }
+}
+
+export default Main;
